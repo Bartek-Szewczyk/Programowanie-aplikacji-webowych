58760218081398e8af00aea5dc4f38919b362835
Bartek-Szewczyk <ba20001000@gmail.com>
2021-03-29 16:05:46 +0200

    add imagine

--- kolko_i_krzyzyk/src/Cell.ts
+++ kolko_i_krzyzyk/src/Cell.ts
@@ -9,12 +9,20 @@ export class Cell {
 
     setCellValue(value:number){
         // TODO: napisz samemu metodę, która ustawia w tabeli kółko i krzyżyk. Możesz przyjąć, że kółko to wartość -1, krzyżyk to wartość 1, a 0 (zero) oznacza pole nieustawione.
+
+        const ximg = new Image();
+        const oimg = document.createElement("img");
+        ximg.src="../Media/x.png";
+        oimg.src="../Media/O.png";
+        ximg.classList.add("img");
+        oimg.classList.add("img");
+
         if (value == 1 && this.cellValue == 0) {
             this.cellValue = 1;
-            this.htmlElement.innerHTML = "X"
+            this.htmlElement.appendChild(ximg);
         }else if (value == -1 && this.cellValue ==0){
             this.cellValue = -1;
-            this.htmlElement.innerHTML = "O"
+            this.htmlElement.appendChild(oimg)
         }
 
     }
