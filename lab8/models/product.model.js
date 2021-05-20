372487f53976e00da24dfd11010efac65ec15795
Bartek-Szewczyk <ba20001000@gmail.com>
2021-05-20 15:19:33 +0200

    lab8

--- lab8/models/product.model.js
+++ lab8/models/product.model.js
@@ -1,5 +1,5 @@
 module.exports = class Product {
-    constructor(name, description, price){
+    constructor(name, description, price) {
         this.name = name;
         this.description = description;
         this.price = price;
