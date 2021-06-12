0f68a37af5bbfa6501cf3051831b22be22839261
Bartek-Szewczyk <ba20001000@gmail.com>
2021-06-12 21:13:29 +0200

    rest api done

--- lab8/app.js
+++ lab8/app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-//app.get('/', (req, res) => res.send('Hello World!'));
 
 require('./routes/product.routes')(app);
+require('./routes/category.routes')(app);
 
 
 app.listen(8080, () => console.log('Server started'));
